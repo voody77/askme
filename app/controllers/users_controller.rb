1a550b46589fbf7eae0b8eaac22d021a10d3c6e9
voody77 <vadim@kruchinin.moscow>
2019-12-14 18:42:41 +0300

    Add Views, helpers[pluralize], add JQuery into webpacker

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'voody77',
+        avatar_url: 'https://www.gravatar.com/avatar/5f16d3f6fbbfe055fc34814ef7e88198?s=200.jpg'
+      ),
+      User.new(
+        id: 2,
+        name: 'Dina',
+        username: 'dinulik',
+        )
+    ]
   end
 
   def new
@@ -12,7 +25,97 @@ class UsersController < ApplicationController
     @user = User.new(
       name: 'Vadim',
       username: 'voody77',
-      avatar_url: 'https://www.gravatar.com/avatar/5f16d3f6fbbfe055fc34814ef7e88198'
+      avatar_url: 'https://www.gravatar.com/avatar/5f16d3f6fbbfe055fc34814ef7e88198?s=200.jpg'
     )
+
+    @questions = [
+      Question.new(
+        text: 'Как дела?',
+        created_at: Date.parse('10.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),Question.new(
+      text: 'Сколько лет нашей планете??',
+      created_at: Date.parse('18.12.2019')
+    ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),Question.new(
+      text: 'Сколько лет нашей планете??',
+      created_at: Date.parse('18.12.2019')
+    ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),
+      Question.new(
+        text: 'Сколько лет нашей планете??',
+        created_at: Date.parse('18.12.2019')
+      ),Question.new(
+      text: 'Сколько лет нашей планете??',
+      created_at: Date.parse('18.12.2019')
+    )
+    ]
+
+    @new_question = Question.new
   end
 end
