724cd1c2230174e0b86bd21f3f7beb37d8cb109b
voody77 <vadim@kruchinin.moscow>
2019-12-15 00:33:28 +0300

    Add actions to User, views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,121 +1,48 @@
 class UsersController < ApplicationController
+
+  before_action :load_user, except: [:index, :create, :new]
+
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Vadim',
-        username: 'voody77',
-        avatar_url: 'https://www.gravatar.com/avatar/5f16d3f6fbbfe055fc34814ef7e88198?s=200.jpg'
-      ),
-      User.new(
-        id: 2,
-        name: 'Dina',
-        username: 'dinulik',
-        )
-    ]
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url, notice: "Пользователь успешно зарегистрирован"
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "Профиль успешно обновлен"
+    else
+      render 'edit'
+    end
   end
 
   def edit
   end
 
   def show
-    @user = User.new(
-      name: 'Vadim',
-      username: 'voody77',
-      avatar_url: 'https://www.gravatar.com/avatar/5f16d3f6fbbfe055fc34814ef7e88198?s=200.jpg'
-    )
+    @questions = @user.questions.order(created_at: :desc)
+    @new_question = @user.questions.build
+  end
 
-    @questions = [
-      Question.new(
-        text: 'Как дела?',
-        created_at: Date.parse('10.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),Question.new(
-      text: 'Сколько лет нашей планете??',
-      created_at: Date.parse('18.12.2019')
-    ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),Question.new(
-      text: 'Сколько лет нашей планете??',
-      created_at: Date.parse('18.12.2019')
-    ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),
-      Question.new(
-        text: 'Сколько лет нашей планете??',
-        created_at: Date.parse('18.12.2019')
-      ),Question.new(
-      text: 'Сколько лет нашей планете??',
-      created_at: Date.parse('18.12.2019')
-    )
-    ]
+  private
+  
+  def load_user
+    @user ||= User.find params[:id]
+  end
 
-    @new_question = Question.new
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :name, :username, :avatar_url)
   end
 end
