aec130e4e04f209f915eabf96434f16473a0fe84
voody77 <vadim@kruchinin.moscow>
2019-12-14 19:17:17 +0300

    Refactor validates in users_controller, users.css

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,33 +7,17 @@ class User < ApplicationRecord
 
   has_many :questions
 
-  validates :email, :username, presence: true, uniqueness: true
-  validates :email, 'valid_email_2/email': true
-  validates :username, length: {minimum: 3, maximum: 40}
-  validates :username, format: {
+  attr_accessor :password
+
+  validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
+  validates :username, presence: true, length: {minimum: 3, maximum: 40}, format: {
     with: /\A\w+\z/,
     message: "only allows letters, numbers or _"
   }
+  validates :password, presence: true, on: :create, confirmation: true
 
-  attr_accessor :password
-
-  validates_presence_of :password, presence: true, on: :create
-  validates_confirmation_of :password
-
-  before_validation :downcase!, uniqueness: true
+  before_validation :downcase!
   before_save :encrypt_password
-
-  def encrypt_password
-    if password.present?
-      # Создаем соль - рандомная строка, усложняющая энкрипт паролей
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      # Создаем хэш пароля - длинная уникальная строка, из которой невозмодно восстановить исходный пароль
-      self.password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
-        password, password_salt, ITERATIONS, DIGEST.length, DIGEST)
-      )
-    end
-  end
 
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
@@ -53,6 +37,18 @@ class User < ApplicationRecord
 
     return user if user.password_hash == hashed_password
     nil
+  end
+
+  def encrypt_password
+    if password.present?
+      # Создаем соль - рандомная строка, усложняющая энкрипт паролей
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      # Создаем хэш пароля - длинная уникальная строка, из которой невозмодно восстановить исходный пароль
+      self.password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
+        password, password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
   end
 
   private
