51bc658bf07fd2494293a05c6678370543a3a002
voody77 <vadim@kruchinin.moscow>
2019-12-15 01:50:42 +0300

    Add sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,19 @@
 class UsersController < ApplicationController
 
   before_action :load_user, except: [:index, :create, :new]
+  before_action :authorize_user, except: [:index, :new, :create, :show]
 
   def index
     @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже вошли' if current_user.present?
     @user = User.new
   end
 
   def create
+    redirect_to root_url, alert: 'Вы уже вошли' if current_user.present?
     @user = User.new(user_params)
     if @user.save
       redirect_to root_url, notice: "Пользователь успешно зарегистрирован"
@@ -36,6 +39,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
   
   def load_user
     @user ||= User.find params[:id]
