e6bd7846c4542542462ac35dd5c76ce5c7c9d6ec
voody77 <vadim@kruchinin.moscow>
2019-12-16 18:38:47 +0300

    Add REGEXP validates bgcolor in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
     message: "only allows letters, numbers or _"
   }
   validates :password, presence: true, on: :create, confirmation: true
+  validates :bgcolor, format: { with: /\A#[\h]{6}\z/ }, allow_nil: true
 
   before_validation :downcase!
   before_save :encrypt_password
