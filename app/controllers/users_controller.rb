6da83c0045b391e4108a2d7e5758a8b687d18590
voody77 <vadim@kruchinin.moscow>
2019-12-17 00:35:19 +0300

    Refactor users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,16 @@ class UsersController < ApplicationController
   end
 
   def new
-    redirect_to user_path, alert: 'Вы уже вошли' if current_user.present?
+    redirect_to root_url, alert: 'Вы уже вошли' if current_user.present?
     @user = User.new
   end
 
   def create
-    redirect_to root_path, alert: 'Вы уже вошли' if current_user.present?
+    redirect_to root_url, alert: 'Вы уже вошли' if current_user.present?
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "Пользователь успешно зарегистрирован"
+      redirect_to root_url, notice: "Пользователь успешно зарегистрирован"
     else
       render 'new'
     end
